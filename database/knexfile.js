797460ef5741fe41cf516b70dda38d3ac5227040
Wojtek <wojtekgg93@gmail.com>
2023-02-12 20:18:24 +0100

    ssl mode set to true

--- database/knexfile.js
+++ database/knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL + '?sslmode=no-verify',
+        connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
             max: 10
