560a4604bcf6845f70975c06301fcba86bc5a884
Wojtek <wojtekgg93@gmail.com>
2023-05-16 11:17:59 +0200

    fixed error that prevent adding image to s3 bucket

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({
         accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     },
-    region: 'eu-north-1'
+    region: 'eu-west-2'
 })
 
 const getURL = async (req, res) => {
